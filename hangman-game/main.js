d740770fd7426494800874fc8ec75ea4592be0bc
John Firth <johfirth@gmail.com>
2018-03-04 18:15:12 -0500

    final changes

--- hangman-game/main.js
+++ hangman-game/main.js
@@ -1,16 +1,16 @@
-var words = ["duck","fish","cat","dog","horse","donkey","pig","cow","goat","chicken"
+var words = ["duck","fish","cat","dog","horse","donkey","pig","cow","goat","chicken", "lamb", "calf", "sheep"
 ]
 
 
 var selectedWord = words[Math.floor(Math.random() * words.length)];
 var dashes = [];
 var alreadyGuessed = [];
-var correctGuesses = [];
 var splitWord = selectedWord.split('');
 var currentResultEl = document.getElementById('current-result');
 var guessesEl = document.getElementById('guessed');
 var guessesRemainEl = document.getElementById('guess-remain');
 var okLetters = 'abcdefghijklmnopqrstuvwxyz'
+var maxGuess = splitWord.length*2
 
 function classFor(letter) {
       return 'letter_' + letter;
@@ -27,6 +27,7 @@ for (var i = 0; i < dashes.length; i++){
     currentResultEl.appendChild(dashes[i]);
 }
 
+guessesRemainEl.innerText = maxGuess +" Guesses"
 
 document.onkeyup = function(event) {
     var key = event.key.toLowerCase();
@@ -39,14 +40,10 @@ document.onkeyup = function(event) {
         return;
     }
 
-    if(splitWord.indexOf(key) !== -1){
-        correctGuesses.push(key);
+    if (alreadyGuessed.length === (maxGuess - 1)){
+        alert("You Lost! The word was: " + selectedWord + ", Try Again")
     }
 
-    if (correctGuesses.length === dashes.length){
-        alert("You Won! The word is:" + selectedWord);
-    }
-    
 
 
     alreadyGuessed.push(key);
@@ -56,7 +53,11 @@ document.onkeyup = function(event) {
     var blanks = document.getElementsByClassName(keyClass)
     for (var i = 0; i < blanks.length; i++) {
         blanks[i].innerHTML = key;
+        blanks[i].classList.add('correct')
+    }
 
+    if (document.getElementsByClassName('correct').length === dashes.length) {
+        alert("You Won! The word was: " + selectedWord); 
     }
     
 
